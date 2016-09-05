f33634c094c09551c8375dcab177846454590dd5
Piotr Muzyka <piotrmuzyka@yahoo.com>
2016-09-06 00:03:19 +0200

    Bug no N/A for out of bound values resolved

--- src/FactorialExercise/FactorialCalculation.java
+++ src/FactorialExercise/FactorialCalculation.java
@@ -3,12 +3,12 @@ package FactorialExercise;
 public class FactorialCalculation {
 	public static long Factorial(int n) {
 		long output = 1;
-
 		if (n >= 0 && n <= 14) {
 			for (int i = 1; i <= n; i++) {
 				output *= i;
 			}
 		} else {
+			output = 0;
 			System.out.print("Value " + n + " is out of bounds [0,..,14]. ");
 		}
 		return output;
