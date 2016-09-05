2e508af6f633cbb77b0ae77206af279c49a5435a
Piotr Muzyka <piotrmuzyka@yahoo.com>
2016-09-05 23:51:37 +0200

    CSVRead class Test added. Code cleaning.

--- src/FactorialExercise/CSVRead.java
+++ src/FactorialExercise/CSVRead.java
@@ -8,22 +8,21 @@ import java.util.List;
 import com.opencsv.CSVReader;
 
 public class CSVRead {
-	 public static List<Integer> InputValues(){
-		  List<Integer> result = new ArrayList<Integer>(); 
-	      try {
-	    	  CSVReader data = new CSVReader(new FileReader("/Users/PiotrM/workspace/csv/input.csv"), ',' , '"' , 0);
-	    	  String[] nextLine;
-	    	  //Read CSV line by line and use the string array as you want
-	      while ((nextLine = data.readNext()) != null) {
-	         if (nextLine != null) {
-	            System.out.println(Arrays.toString(nextLine));
-	            result.add(Integer.parseInt(nextLine[0]));
-	         }
-	      }
-	      data.close();
-	      }
-	      catch(Exception e) {
-	      }	      
-	      return result;
-	 }	      
+	public static List<Integer> InputValues() {
+		List<Integer> result = new ArrayList<Integer>();
+		try {
+			CSVReader data = new CSVReader(new FileReader("/Users/PiotrM/workspace/csv/input.csv"), ',', '"', 0);
+			String[] nextLine;
+			// Read CSV line by line and use the string array as you want
+			while ((nextLine = data.readNext()) != null) {
+				if (nextLine != null) {
+					System.out.println(Arrays.toString(nextLine));
+					result.add(Integer.parseInt(nextLine[0]));
+				}
+			}
+			data.close();
+		} catch (Exception e) {
+		}
+		return result;
+	}
 }
--- src/FactorialExercise/TestClass.java
+++ src/FactorialExercise/TestClass.java
@@ -5,15 +5,16 @@ import org.junit.Test;
 
 public class TestClass {
 	// FactorialClass test
-	
+
 	// assert statements // Boundary Value Analysis
 	@Test
-	 public void factorialOfZeroShouldReturnOne() {   
-       assertEquals("0! must be 1", 1, FactorialCalculation.Factorial(0));
+	public void factorialOfZeroShouldReturnOne() {
+		assertEquals("0! must be 1", 1, FactorialCalculation.Factorial(0));
 	}
-	@Test 
-	 public void factorialOfFourteenShouldReturn87178291200() {
-      assertEquals("14! must be 87178291200", 87178291200L, FactorialCalculation.Factorial(14));
+
+	@Test
+	public void factorialOfFourteenShouldReturn87178291200() {
+		assertEquals("14! must be 87178291200", 87178291200L, FactorialCalculation.Factorial(14));
 	}
 
 }
